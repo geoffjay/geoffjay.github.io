6092f723caf41ee743976b57ab6539b8453c361c
Geoff Johnson <geoff.jay@gmail.com>
2021-11-14 14:24:08 -0800

    feat: mask loader

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   webpack: (config, options) => {
     return config;
   },
   reactStrictMode: true,
+  assetPrefix: isProd ? "https://geoffjay.github.io/" : "",
   images: {
-    disableStaticImages: true,
-    // fake the third-party loader
     loader: "imgix",
     path: "",
   },
