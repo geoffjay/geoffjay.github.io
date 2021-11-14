16e43f87751a0e64df04c5cce13adae5ce1feb3f
Geoff Johnson <geoff.jay@gmail.com>
2021-11-14 13:10:59 -0800

    fix: export error

--- next.config.js
+++ next.config.js
@@ -1,3 +1,29 @@
-module.exports = {
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
+
+const nextConfig = {
+  webpack: (config, options) => {
+    return config;
+  },
   reactStrictMode: true,
+  images: {
+    disableStaticImages: true,
+    // fake the third-party loader
+    loader: "imgix",
+    path: "",
+  },
 };
+
+const config = withPlugins(
+  [
+    [
+      optimizedImages,
+      {
+        optimizeImages: false,
+      },
+    ],
+  ],
+  nextConfig,
+);
+
+module.exports = config;
