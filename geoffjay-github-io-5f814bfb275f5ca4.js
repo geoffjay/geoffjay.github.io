34a8c4ea0411abc2f1ae29d41ba08879e2e1805f
geoffjay <geoffjay@users.noreply.github.com>
2023-12-20 20:57:12 +0000

    deploy: efdeffd45bfbfde61b3939c8a0b3ac7a1b0d15b1

--- geoffjay-github-io-5f814bfb275f5ca4.js
+++ geoffjay-github-io-5f814bfb275f5ca4.js
@@ -988,7 +988,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('geoffjay-github-io-69962f71bf5f7aa2_bg.wasm', import.meta.url);
+        input = new URL('geoffjay-github-io-5f814bfb275f5ca4_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
